ebb8edbd72682f6816bca67e3a4dc837087eb441
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-01-17 16:58:25 +0900

    Add recipe for installing 'yum-epel'

--- recipes/yum-repos.rb
+++ recipes/yum-repos.rb
@@ -1,9 +1,11 @@
 #
 # Cookbook Name:: prepare-env
-# Recipe:: default
+# Recipe:: yum-repos
 # Author:: Fumihiko Shiroyama<fu.shiroyama@gmail.com>
 #
 # Copyright (C) 2014 Fumihiko Shiroyama
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 #
+
+include_recipe "yum-epel"
