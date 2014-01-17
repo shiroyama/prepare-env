916229555b2ef6096d869c642e14a9f25934f7f7
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-01-17 17:13:29 +0900

    Modify metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'prepare-env'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Fumihiko Shiroyama'
+maintainer_email 'fu.shiroyama@gmail.com'
+license          'Apache 2.0'
 description      'Installs/Configures prepare-env'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
