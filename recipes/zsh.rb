5fa5c10c94207e282259f759f22a8fdf894577fc
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-01-23 16:07:18 +0900

    Add role "prepare", "rails" to setup rails environment

---
+++ recipes/zsh.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: prepare-env
+# Recipe:: zsh
+# Author:: Fumihiko Shiroyama<fu.shiroyama@gmail.com>
+#
+# Copyright (C) 2014 Fumihiko Shiroyama
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#
+
+include_recipe 'prepare-env::yum-repos'
+
+package "zsh" do
+    action :install
+end
