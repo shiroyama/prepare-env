897fb5ae9cd98543ad9653d6c5d41b9e6cf4ed70
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-01-23 18:37:36 +0900

    Remove nginx, nginx-rails attribute from recipe as attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,9 @@
+### rbenv
 default["rbenv"]["version"] = "2.0.0-p353"
+
+### nginx
+default["nginx"]["port"]     = "80"
+default["nginx"]["upstream"] = "unicorn"
+
+### rails
+default["rails"]["server"]["socket"] = "unix:/tmp/unicorn.sock"
