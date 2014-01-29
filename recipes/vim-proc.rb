13c8fa0f78bfd730f822ec9c9906ee7e938b2417
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-01-29 19:38:29 +0900

    Modify vim recipe to compile vimproc

---
+++ recipes/vim-proc.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: prepare-env
+# Recipe:: vim-proc
+# Author:: Fumihiko Shiroyama<fu.shiroyama@gmail.com>
+#
+# Copyright (C) 2014 Fumihiko Shiroyama
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#
+
+include_recipe 'prepare-env::vim'
+
+default_user = data_bag_item("users", "default")
+
+execute "compile vimproc" do
+    command "su #{default_user["name"]} -l -c \"cd #{default_user['home']}/.bundle/vimproc && make 1>/dev/null 2>&1\""
+    only_if do
+        /(yes|^y)/i =~ node[:vim][:vimproc][:compile]
+    end
+end
