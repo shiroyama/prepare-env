28eac1dc90df1d0e410b28ff9b6d8b8312397c9b
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-03-06 17:01:52 +0900

    Modify redis recipe to start automatically

--- recipes/redis.rb
+++ recipes/redis.rb
@@ -13,3 +13,8 @@ include_recipe 'prepare-env::yum-repos'
 package "redis" do
     action :install
 end
+
+service "redis" do
+    supports :status => true, :restart => true, :reload => true
+    action [:enable, :start]
+end
