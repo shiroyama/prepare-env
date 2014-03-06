827d95d144f2285b1e527a57de8a3d86d0323e99
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-03-06 17:06:00 +0900

    Add tree, lsof installations

---
+++ recipes/lsof.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: prepare-env
+# Recipe:: lsof
+# Author:: Fumihiko Shiroyama<fu.shiroyama@gmail.com>
+#
+# Copyright (C) 2014 Fumihiko Shiroyama
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#
+
+package "lsof" do
+    action :install
+end
---
+++ recipes/tree.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: prepare-env
+# Recipe:: tree
+# Author:: Fumihiko Shiroyama<fu.shiroyama@gmail.com>
+#
+# Copyright (C) 2014 Fumihiko Shiroyama
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#
+
+package "tree" do
+    action :install
+end
